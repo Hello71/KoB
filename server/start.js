f23db6526fcd355d58b0893bf654ad8e51d568f5
Alex Xu <alex_y_xu@yahoo.ca>
2012-02-01 17:49:14 -0500

    optional config.json

--- server/start.js
+++ server/start.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 "use strict";
 var fs = require("fs"),
-    config = JSON.parse(fs.readFileSync("config.json", "utf-8"));
+    configFile = "config.json",
+    config = {};
 
+if (fs.existsSync(configFile)) {
+    config = JSON.parse(fs.readFileSync("config.json", "utf-8"));
+}
 require("./server.js").start(config);
